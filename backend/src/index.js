c00653fa4ace8fdecbfd2e76bf3a751cf61985df
Frank <codingfrank73@gmail.com>
2022-05-12 16:00:17 +0200

    cors wiederhergestellt

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,24 +9,22 @@ dotenv.config()
 const PORT = process.env.PORT || 9000;
 const app = express();
 
-app.use(cors())
-// app.use(cors({ origin: [process.env.FRONTEND_URL], credentials: true }))
+app.use(cors({ origin: [process.env.FRONTEND_URL], credentials: true }))
 
-// const oneDayInMs = 24 * 60 * 60 * 1000;
-// const isLocalHost = process.env.FRONTEND_URL === 'http://localhost:3000';
+const oneDayInMs = 24 * 60 * 60 * 1000;
+const isLocalHost = process.env.FRONTEND_URL === 'http://localhost:3000';
 
-// app.set('trust proxy', 1);
-// app.use(
-//     cookieSession({
-//         name: 'session',
-//         secret: process.env.COOKIE_SESSION_SECRET,
-//         httpOnly: true,
-//         expires: new Date(Date.now() + oneDayInMs),
-//         sameSite: isLocalHost ? 'lax' : 'none',
-//         secure: isLocalHost ? false : true,
-//     })
-// )
-
+app.set('trust proxy', 1);
+app.use(
+    cookieSession({
+        name: 'session',
+        secret: process.env.COOKIE_SESSION_SECRET,
+        httpOnly: true,
+        expires: new Date(Date.now() + oneDayInMs),
+        sameSite: isLocalHost ? 'lax' : 'none',
+        secure: isLocalHost ? false : true,
+    })
+)
 
 app.use(morgan("dev"));
 app.use(express.json());
