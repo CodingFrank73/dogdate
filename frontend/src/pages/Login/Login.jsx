77eb781b3813c0ea2b644e41b2eb69e08ea12ca8
Frank <codingfrank73@gmail.com>
2022-05-15 19:51:01 +0200

    cors sucks...

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -15,6 +15,7 @@ const Login = (props) => {
 
             const response = await fetch(apiBaseUrl + '/api/users/login', {
                 method: "POST",
+                mode: "same-origin"
                 // headers: { "Content-Type": "application/json" },
                 credentials: "include",
                 body: JSON.stringify({ email, password })
