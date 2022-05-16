b0b981578e3997d92ab97ca2d2d5f5175454cf46
Frank <codingfrank73@gmail.com>
2022-05-16 11:14:19 +0200

    cors läuft...

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 9000;
 const app = express();
 
 // app.use(cors({ origin: true, credentials: true }))
-app.use(cors({ origin: [process.env.FRONTEND_URL], credentials: true }))
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }))
 
 console.log(
     "Frontend_URL:", process.env.FRONTEND_URL
