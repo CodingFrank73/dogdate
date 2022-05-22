7d79160ea6557f8688694b43d5bef7a082da1202
G-ManGuido <supercoder@AirvonSperCoder.fritz.box>
2022-05-22 12:32:00 +0200

    Swipe Cards

--- frontend/src/pages/Home/SuggestionCard.jsx
+++ frontend/src/pages/Home/SuggestionCard.jsx
@@ -1,16 +1,16 @@
 
 const SuggestionCard = (props) => {
     console.log("Propos in SuggestionCard: ", props)
-    const x = props.wert + 1
+    const x = props.wert + 2
 
     return (
-        <div>
-            <div id={`dog-id${x}`} className={`dog-wrapper${props.wert}`} style={{ zIndex: `${x}` }}>
-                <img src={`dogs/${props.suggestion.dogName}.png`} alt="dog pic" />
-                <div className="dogName">{props.suggestion.dogName}, {props.suggestion.age}</div>
-                <div className="distanceKM">{props.suggestion.maxDistance} km</div>
-            </div>
-        </div >
+
+        <div id={`dog-id${x}`} className={`dog-wrapper0${x}`} style={{ zIndex: `${x}` }}>
+            <img src={`dogs/${props.suggestion.dogName}.png`} alt="dog pic" />
+            <div className="dogName">{props.suggestion.dogName}, {props.suggestion.age}</div>
+            <div className="distanceKM">{props.suggestion.maxDistance} km</div>
+        </div>
+
     );
 }
 
--- frontend/src/pages/Home/SuggestionsStaple.jsx
+++ frontend/src/pages/Home/SuggestionsStaple.jsx
@@ -3,7 +3,7 @@ import SuggestionCard from "./SuggestionCard";
 
 const SuggestionsStaple = (props) => {
     return (
-        <div>
+        <div className="home-doggy-bigpic">
             {props.suggestions.map((suggestion, i) => <SuggestionCard suggestion={suggestion} wert={i} />)}
         </div>
     );
