6b730d256f6fedbff1494d2efe41e85089e624eb
G-ManGuido <supercoder@AirvonSperCoder.fritz.box>
2022-05-22 10:57:05 +0200

    mehr bilder und css

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -12,6 +12,7 @@ import dogImage01 from '../../assets/img/dimka.png';
 import dogImage02 from '../../assets/img/goliath.png';
 import dogImage03 from '../../assets/img/idefix.png';
 import dogImage04 from '../../assets/img/sandy.png';
+import noMore from '../../assets/img/no-more.gif';
 
 import ddLogo from '../../assets/icons/logo.svg';
 import filter from '../../assets/icons/filter.svg';
@@ -21,6 +22,7 @@ import iconHomeaktiv from '../../assets/icons/home-aktiv.svg';
 import iconLike from '../../assets/icons/like.svg';
 import iconChat from '../../assets/icons/chat.svg';
 import iconProfile from '../../assets/icons/profile.svg';
+
 
 import apiBaseUrl from "../../api"
 
@@ -202,23 +204,24 @@ const Home = (props) => {
                 </div>
 
                 <div className="home-doggy-bigpic">
-                    <div className="dog-wrapper01">
+                    <div className="dog-wrapperBackground">
+                        <img src={noMore} alt="dog pic" />
+                        <div className="textL1">Sorry, no more</div>
+                        <div className="textL2">suggestions available.</div>
+                    </div>
+
+                    <div className="dog-wrapper02">
                         <img src={dogImage01} alt="dog pic" />
                         <div className="dogName">Dimka, 5</div>
                         <div className="distanceKM">4 km</div>
                     </div>
-                    <div className="dog-wrapper02">
+                    <div className="dog-wrapper03">
                         <img src={dogImage02} alt="dog pic" />
                         <div className="dogName">Goliath, 8</div>
                         <div className="distanceKM">15 km</div>
                     </div>
-                    <div className="dog-wrapper03">
-                        <img src={dogImage03} alt="dog pic" />
-                        <div className="dogName">Idefix, 12</div>
-                        <div className="distanceKM">1 km</div>
-                    </div>
                     <div className="dog-wrapper04">
-                        <img src={dogImage04} alt="dog pic" />
+                        <img src="dogs/balu.png" alt="dog pic" />
                         <div className="dogName">Sandy, 2</div>
                         <div className="distanceKM">8 km</div>
                     </div>
