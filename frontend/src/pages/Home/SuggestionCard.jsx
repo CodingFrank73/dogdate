3c623c5bf4c35cca962bb8c50a2f5a90e89ccfd1
Frank <codingfrank73@gmail.com>
2022-05-22 16:43:13 +0200

    bild sichtbar

--- frontend/src/pages/Home/SuggestionCard.jsx
+++ frontend/src/pages/Home/SuggestionCard.jsx
@@ -3,7 +3,7 @@ const SuggestionCard = (props) => {
     const x = props.wert + 2
 
     return (
-        <div id={`dog-id${x}`} className={`dog-wrapper01`} style={{ zIndex: `${x}` }}>
+        <div id={`dog-id${x}`} className={`dog-wrapper0${x}`} style={{ zIndex: `${x}` }}>
             <img src={`dogs/${props.suggestion.dogName}.png`} alt="dog pic" />
             <div className="dogName">{props.suggestion.dogName}, {props.suggestion.age}</div>
             <div className="distanceKM">{props.suggestion.maxDistance} km</div>
